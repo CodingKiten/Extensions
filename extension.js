9fd341981097203fd38048f95707835ff2460ff5
CodingKiten <121097932+CodingKiten@users.noreply.github.com>
2023-08-14 11:54:55 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,1 +1,32 @@
+class ColorChanger {
+  constructor(runtime) {
+    this.runtime = runtime;
+  }
+  
+  getInfo() {
+    return {
+      id: 'color-changer',
+      name: 'Color Changer',
+      blocks: [
+        {
+          opcode: 'changeStageColor',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Change stage color to [COLOR]',
+          arguments: {
+            COLOR: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'red'
+            }
+          }
+        }
+      ]
+    };
+  }
+  
+  changeStageColor(args) {
+    const color = args.COLOR;
+    document.body.style.backgroundColor = color;
+  }
+}
 
+Scratch.extensions.register(new ColorChanger(Scratch.runtime));
